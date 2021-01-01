0dd80e2cbeaa4e0d744bc29a6d5514bfec707a13
Juan J. Garcia <juanjo.garcia@gmail.com>
2021-01-02 00:36:56 +0100

    master: Demo with callback

---
+++ src/main/java/com/github/j2gl/kafka/tutorial1/ProducerDemoWithCallback.java
@@ -0,0 +1,51 @@
+package com.github.j2gl.kafka.tutorial1;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerDemoWithCallback {
+
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+
+        final String bootstrapServers = "127.0.0.1:9092";
+
+        // create producer properties
+        final Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        final KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "Hello World! " + i);
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e == null) {
+                        logger.info("Received new metadata. \n" +
+                            "Topic    : " + recordMetadata.topic() + "\n" +
+                            "Partition: " + recordMetadata.partition() + "\n" +
+                            "Offset   : " + recordMetadata.offset() + "\n" +
+                            "Timestamp: " + recordMetadata.timestamp() + "\n");
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }
+            });
+            producer.flush();
+        }
+
+        producer.close();
+    }
+}

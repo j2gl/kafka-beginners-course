85223b7b97e096994a188d452ecf1df090556b46
Juan J. Garcia <juanjo.garcia@gmail.com>
2021-01-02 00:19:53 +0100

    master: Initial commit

---
+++ src/main/java/com/github/j2gl/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,30 @@
+package com.github.j2gl.kafka.tutorial1;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+
+        final String bootstrapServers = "127.0.0.1:9092";
+
+        // create producer properties
+        final Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        final KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "Hello World!");
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
+    }
+}
